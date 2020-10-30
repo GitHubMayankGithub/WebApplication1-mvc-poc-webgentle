474b36192578e25db5449cf7f6ce1a90f7a56cef
GitHubMayankGithub <explorermayank94@gmail.com>
2020-10-30 11:54:10 +0530

    Employee Controller changes

---
+++ WebApplication1 mvc poc webgentle/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1_mvc_poc_webgentle.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public string EmployeeProfile(int Id)
+        {
+            string profile = "";
+            if(Id==1)
+            {
+                return "profile of user-1";
+            }
+            else if(Id==2)
+            {
+                return "profile of user-2";
+            }
+            else if (Id == 3)
+            {
+                return "profile of user-3";
+            }
+            else
+            {
+                return "no user found";
+            }
+          
+        }
+    }
+}
