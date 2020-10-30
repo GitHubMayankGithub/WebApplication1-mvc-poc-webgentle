3c0b5a364d4e8b3e3b4d8fd78f2f97183152bab8
GitHubMayankGithub <explorermayank94@gmail.com>
2020-10-30 11:21:50 +0530

    adding controller MyHome

---
+++ WebApplication1 mvc poc webgentle/Controllers/MyHomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1_mvc_poc_webgentle.Controllers
+{
+    public class MyHomeController : Controller
+    {
+        // GET: MyHome
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        public string  Index()
+        {
+            return "hellow world";
+        }
+
+        public string Name()
+        {
+            return "Mayank";
+        }
+    }
+}
