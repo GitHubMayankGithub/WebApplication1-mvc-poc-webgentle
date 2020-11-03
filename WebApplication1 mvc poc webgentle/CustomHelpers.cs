98bed96c2bd12c6df7d1b0a5c2c76c751977389e
GitHubMayankGithub <explorermayank94@gmail.com>
2020-11-03 09:32:15 +0530

    custom helper-image string

---
+++ WebApplication1 mvc poc webgentle/CustomHelpers.cs
@@ -0,0 +1,17 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1_mvc_poc_webgentle
+{
+    public static class CustomHelpers
+    {
+
+        public static IHtmlString Image(string src,string alt)
+        {
+            return new MvcHtmlString(string.Format("<img src='{0}' alt='{1}'</img>", src, alt));
+        }
+    }
+}
