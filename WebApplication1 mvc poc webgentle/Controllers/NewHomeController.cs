48cd8788f810840c09c5fa7af281ad5747a09209
GitHubMayankGithub <explorermayank94@gmail.com>
2020-11-04 13:09:15 +0530

    sending data from view to contoller using REQUEST.FORM-COLLECTION,P ARAMTERS ADN JS

---
+++ WebApplication1 mvc poc webgentle/Controllers/NewHomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1_mvc_poc_webgentle.Models;
+namespace WebApplication1_mvc_poc_webgentle.Controllers
+{
+    public class NewHomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// using post parametrs
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public string PostUsingParameters(string firstName, string lastName)
+        {
+            return "From Parameters " + firstName + "  " + lastName + " ";
+        }
+
+        /// <summary>
+        /// using request paramter
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public string PostUsingRequest()
+        {
+            string firstName=Request["firstName"];
+            string lastName = Request["lastName"];
+            return "From PostUsingRequest " + firstName + "  " + lastName + " ";
+        }
+        
+        
+        [HttpPost]
+        public string PostUsingFormCollection(FormCollection form)
+        {
+            string firstName= form["firstName"];
+            string lastName = form["lastName"];
+            return "From PostUsingFormCollection " + firstName + "  " + lastName + " ";
+        }
+
+        //4th CASE IS USING STRONGLY BINDING TO POPST DATA FROM VIEW TO CONTROLLER. BY CREATING MODEL.
+        /// <summary>
+        /// PostUsingStronglyBinding
+        /// </summary>
+        /// <param name="form"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public string PostUsingStronglyBinding(NewEmployee emp)
+        {
+            return "From PostUsingStronglyBinding " + emp.firstName + "  " + emp.lastName + " ";
+        }
+
+    }
+}
---
+++ WebApplication1 mvc poc webgentle/Models/NewEmployee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1_mvc_poc_webgentle.Models
+{
+    public class NewEmployee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
