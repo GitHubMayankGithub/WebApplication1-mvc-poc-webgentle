86e440e3be0f90462e49ac8b3b24cc5565503408
GitHubMayankGithub <explorermayank94@gmail.com>
2020-11-10 05:44:21 +0530

    connecting to DB , adding a record , fetching record using navigational properties and foreign key concept for add/get records

--- MyApp.DB/DbOperations/EmployeeRepository.cs
+++ MyApp.DB/DbOperations/EmployeeRepository.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyApp.Model;
 
 namespace MyApp.DB.DbOperations
 {
     public class EmployeeRepository
     {
+
+        public int AddEmployee(EmployeeModel model)
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                Employee employee = new Employee()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    AddressId = model.AddressId,
+                    Email = model.Email,
+                    Code = model.Code,
+                };
+
+                if (model.Address != null)
+                {
+                    employee.Address = new Address()
+                    {
+                        Details = model.Address.Details,
+                        State = model.Address.State,
+                        Country = model.Address.Country
+                    };
+                }
+                context.Employee.Add(employee);
+                context.SaveChanges();
+                return employee.Id;
+            }
+        }
+
+
+        public List<EmployeeModel> GetAllEmployees()
+        {
+            using (var context = new EmployeeDBEntities())
+            {
+                var result = context.Employee.Select(x => new EmployeeModel()
+                {
+                    AddressId = x.AddressId,
+                    Id = x.Id,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Code = x.Code,
+                    Email = x.Email,
+                    Address= new AddressModel()
+                    {
+                        Id=x.Address.Id,
+                        Details=x.Address.Details,
+                        State=x.Address.State,
+                        Country=x.Address.Country
+                    }
+                }).ToList();
+            }
+        }
     }
 }
---
+++ WebApplication1 mvc poc webgentle/Controllers/HomeCRUDController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyApp.Model;
+using MyApp.DB.DbOperations; 
+
+namespace WebApplication1_mvc_poc_webgentle.Controllers
+{
+    public class HomeCRUDController : Controller
+    {
+        EmployeeRepository employeeRepository = null;
+
+        public HomeCRUDController()
+        {
+            employeeRepository = new EmployeeRepository();
+        }
+
+        // GET: HomeCRUD
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(EmployeeModel employeeModel)
+        {
+            if(ModelState.IsValid)
+            {
+                int id=employeeRepository.AddEmployee(employeeModel);
+
+                if (id > 0)
+                {
+                    ModelState.Clear();
+                    ViewBag.IsSuccess = "Data added successfully !";
+                }
+            }
+           
+            return View();
+        }
+    }
+}
